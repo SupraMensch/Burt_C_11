57229161ea723fc1801a8c60d67fb33c0c156357
SupraMensch <cwadeburt@gmail.com>
2018-09-24 15:12:45 -0500

    first draft

---
+++ src/burt_c_11/Burt_C_11.java
@@ -0,0 +1,29 @@
+// Christopher Burt
+// Project # 11
+
+//Write a Customer class that contains the following fields:
+
+//- Name
+//- Customer ID
+//- Address
+//- Phone Number
+
+//The class should have two constructors:
+//- A no-arg constructor that sets the fields to empty strings ("")
+//- A constructor that accepts the four fields as arguments and assigns them to their appropriate fields in the class
+
+//Write appropriate mutator methods to store values in the fields and accessor methods to return the values in the fields.
+
+//Write a separate program that creates three Customer objects by entering the fields for each object from the keyboard.
+
+package burt_c_11;
+
+
+public class Burt_C_11 
+{
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
