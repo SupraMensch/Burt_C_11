796b442575336e70358da1b3d8749782d852759c
Christopher Burt <cwadeburt@gmail.com>
2018-09-28 01:07:58 -0500

    customer entry added

---
+++ src/burt_c_11/UserInput.java
@@ -0,0 +1,13 @@
+// Christopher Burt
+// Project #12
+
+//  This class creates three Customer objects by entering the fields for each object from the
+// keyboard.
+package burt_c_11;
+
+import java.util.Scanner; 
+
+public class UserInput
+{
+    
+}
